4cb79a95ad5544677200297d5af46f586d1a43cb
Rishika Varma K <rishikavarmak@gmail.com>
2023-04-19 21:30:04 +0530

    Initial functional draft for sequential k cores algo

--- K-Cores/kcores.cpp
+++ K-Cores/kcores.cpp
@@ -43,11 +43,6 @@ struct Graph
     adj_list[d].push_back(s);
     degree[s]++;
     degree[d]++;
-    min_degree = min(degree[s], degree[d], min_degree);
-    if (min_degree == degree[s])
-      start_vertex = s;
-    if (min_degree == degree[d])
-      start_vertex = d;
     e++;
   }
 
@@ -78,20 +73,18 @@ void compute_k_cores_dfs(Graph &g, vector<bool> &visited, int v, int k)
   {
     int adj_v = g.adj_list[v][i];
     g.degree[adj_v]--;
-    if (!visited[adj_v])
+    for (int j = 0; j < g.degree[adj_v]; j++)
     {
-      if (g.degree[adj_v] < k)
+      if (g.adj_list[adj_v][j] == v)
       {
-        for (int j = 0; j < g.degree[adj_v]; j++)
-        {
-          if (g.adj_list[adj_v][j] == v)
-          {
-            g.adj_list[adj_v][j] = g.adj_list[adj_v][g.degree[adj_v]];
-            break;
-          }
-        }
-        compute_k_cores_dfs(g, visited, adj_v, k);
+        g.adj_list[adj_v][j] = g.adj_list[adj_v][g.degree[adj_v]];
+        break;
       }
+    }
+    // if(!visited[adj_v]) cout<<adj_v<<endl;
+    if (!visited[adj_v] && g.degree[adj_v] < k)
+    {
+      compute_k_cores_dfs(g, visited, adj_v, k);
     }
   }
 }
@@ -99,16 +92,76 @@ void compute_k_cores_dfs(Graph &g, vector<bool> &visited, int v, int k)
 void compute_k_cores(Graph &g, int k)
 {
   vector<bool> visited(g.v, false);
-  compute_k_cores_dfs(g, visited, g.start_vertex, k);
   for (int i = 0; i < g.v; i++)
   {
+    //  if(!visited[i]) cout<<i<<endl;
     if (!visited[i] && g.degree[i] < k)
     {
+
       compute_k_cores_dfs(g, visited, i, k);
     }
   }
 }
 
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
+  Graph g1(9);
+  g1.addEdge(0, 1);
+  g1.addEdge(0, 2);
+  g1.addEdge(1, 2);
+  g1.addEdge(1, 5);
+  g1.addEdge(2, 3);
+  g1.addEdge(2, 4);
+  g1.addEdge(2, 5);
+  g1.addEdge(2, 6);
+  g1.addEdge(3, 4);
+  g1.addEdge(3, 6);
+  g1.addEdge(3, 7);
+  g1.addEdge(4, 6);
+  g1.addEdge(4, 7);
+  g1.addEdge(5, 6);
+  g1.addEdge(5, 8);
+  g1.addEdge(6, 7);
+  g1.addEdge(6, 8);
+  compute_k_cores(g1, 3);
+  cout << "G1:" << endl;
+  g1.printKCoresGraph(3);
+
+  Graph g2(13);
+  g2.addEdge(0, 1);
+  g2.addEdge(0, 2);
+  g2.addEdge(0, 3);
+  g2.addEdge(1, 4);
+  g2.addEdge(1, 5);
+  g2.addEdge(1, 6);
+  g2.addEdge(2, 7);
+  g2.addEdge(2, 8);
+  g2.addEdge(2, 9);
+  g2.addEdge(3, 10);
+  g2.addEdge(3, 11);
+  g2.addEdge(3, 12);
+  compute_k_cores(g2, 3);
+  cout << "G2:" << endl;
+  g2.printKCoresGraph(3);
+
+  Graph gr(9);
+  gr.addEdge(0, 1);
+  gr.addEdge(0, 2);
+  gr.addEdge(1, 2);
+  gr.addEdge(2, 5);
+  gr.addEdge(2, 4);
+  gr.addEdge(2, 3);
+  gr.addEdge(2, 6);
+  gr.addEdge(3, 4);
+  gr.addEdge(3, 6);
+  gr.addEdge(3, 7);
+  gr.addEdge(4, 6);
+  gr.addEdge(5, 6);
+  gr.addEdge(5, 7);
+  gr.addEdge(5, 8);
+  gr.addEdge(8, 7);
+  compute_k_cores(gr, 3);
+  cout << "G3:" << endl;
+  gr.printKCoresGraph(3);
+  return 0;
 }
